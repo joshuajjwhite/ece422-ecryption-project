c0d4948b374448a76000e0ac882f9a0e5a5c143f
joshuajjwhite <joshua.jj.white@gmail.com>
2017-04-04 17:55:53 -0600

    Succesffully Encrypt and Decrypt with 128 bit keys

---
+++ src/make.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+##!/bin/bash
+
+#INPUT=$1
+#RANDOM_NUMBER=$2
+#OUTPUT=$3
+#PFAIL=$4
+#SFAIL=$5
+#TIMEOUT=$6
+
+javac -classpath ./com/company/commons-io-2.5.jar ./com/company/*.java
+javah -jni com.company.TEA
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:.
+
+rm com/company/libTEA.so
+
+gcc -shared -fpic -o ./com/company/libTEA.so -I/$JAVA_HOME/include -I/$JAVA_HOME/include/linux ./com/company/TEA.c
+
+#java -classpath ./:./commons-io-2.5.jar  DataGenerator $INPUT $RANDOM_NUMBER
+
+#java -classpath ./:./commons-io-2.5.jar SortDriver $INPUT $OUTPUT $PFAIL $SFAIL $TIMEOUT
---
+++ src/run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+java -Djava.library.path=`pwd`/com/company/ com.company.EncryptionDriver
